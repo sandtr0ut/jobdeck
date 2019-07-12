9d004fd2657f09d88ac9cb07e7dd947f5baac2d3
sandtr0ut <crosbyverse@gmail.com>
2019-07-12 00:51:16 -0700

    add NotFound component, separate routes from app.js, add view profile button

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -7,6 +7,9 @@ const DashboardActions = () => {
       <Link to="/edit-profile" className="btn btn-light">
         <i className="fas fa-user-circle text-primary" /> Edit Profile
       </Link>
+      <Link to="/profile/profile.user._id" className="btn btn-light">
+        <i className="fas fa-user-circle text-primary" /> View Profile
+      </Link>
       <Link to="/add-experience" className="btn btn-light">
         <i className="fab fa-black-tie text-primary" /> Add Experience
       </Link>
