96710cb326790b3a6c2497ff988fff13bf251708
Daniel M <mcmanimoncoding@gmail.com>
2019-07-24 19:13:51 -0700

    packagelocks

--- client/src/components/jobcard/dispJobCard.js
+++ client/src/components/jobcard/dispJobCard.js
@@ -13,32 +13,48 @@ import Spinner from '../layout/Spinner';
 import ReactDOM from 'react-dom';
 import CardFront from './cardFront'
 import CardBack from './cardBack'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 // '../../actions/profile';
 
 
-class DispJobCard extends Component{
+class DispJobCard extends Component {
     constructor(props){
         super(props);
 
         this.state={
-            summary: false
+            summary: false,
+
         }
-    
+
     }
-  
-  render() {
 
-    return(
-    <div>
-       <CardFront 
-        
-       />
-       <CardBack
+    handleShowCardBody = () => {
+        this.setState({
+            bodyVisible: !this.state.bodyVisible
+        })
+    }
 
-       />
-    </div>
-    )
-} 
+    render() {
+
+        return (
+            // <div className="card-body" bodyVisible={false}>
+            // <CardFront>
+            // <CardBack />
+            // </CardFront>
+            // </div>
+            <div className="whole-card">
+                <div onClick={this.handleShowCardBody}>{this.props.title}</div>
+                <div className="card-body" bodyVisible={this.state.bodyVisible}>
+                    {this.state.bodyVisible ? this.props.children : null}
+                </div>
+            </div>
+            // <DispJobCard title="This is a title" bodyVisible={false}>
+            //     <p>Everything here is going to appear below the card title.</p>
+            // </DispJobCard>
+        )
+    }
 }
 
 
